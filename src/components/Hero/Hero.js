acfa95703286ff3c8cbf5ba0b190ea6da588ce60
Max Rivera <hellomauriciorivera@gmail.com>
2022-01-05 10:58:28 -0500

    new year save

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,18 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hey there, I'm Max
+      </SectionTitle>
+      <SectionText>
+        Web Designer and Front End Software Engineer
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}> Lets Talk</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
